76b0874c4ced9b2ea10366a35e054156d8b9daf3
Eric Sans <esansvi@gmail.com>
2022-12-21 17:16:32 +0100

    Eric Sans Vidal - working on invoice class

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,5 +1,4 @@
 import java.util.Date;
-import java.util.Calendar;
 
 public class Invoice {
     private InvoiceLine [] lines;
@@ -40,6 +39,7 @@ public class Invoice {
 // FALTE MODIFICAR PER IMPRIMIR-HO TOT BÉ
     public void print(){
         printTop();
+        printInfoTop();
         for (int i=0; i<getLines().length;i++){
             this.lines[i].printLine();
         }
@@ -47,34 +47,103 @@ public class Invoice {
 
     private void printTop() {
         System.out.println("                                                     Invoice number: "+this.invoiceNumber);
-        //System.out.print("                                                     "+
-        //        "                         DATE: ");
-        //System.out.format("%tB %te, %ty%n", this.invoiceDate.getMonth(),this.invoiceDate.getDay(), this.invoiceDate.getYear());
+        System.out.print("                                                     "+
+                "                                                          DATE: ");
+        System.out.format("%tB %td, %tY%n", this.invoiceDate,this.invoiceDate, this.invoiceDate);
         System.out.println("                                                                 To: "+this.invoiceTo);
         System.out.println("                                                            Ship to: "+this.shipTo);
         System.out.println();
-        System.out.println("----------------------------------------------------------------------------------"+
-                "---------------------------------");
+        printDash();
         System.out.print('|');
         System.out.print("        SALES PERSON");
         System.out.print(" | ");
         System.out.print("        SHIPPING METHOD");
         System.out.print(" | ");
-        System.out.print("        SHIPPING TERMS");
+        System.out.print("         SHIPPING TERMS");
         System.out.print(" | ");
         System.out.print("    DELIVERY DATE");
         System.out.print(" | ");
-        System.out.print("        PAYMENT TERMS");
+        System.out.print("          PAYMENT TERMS");
         System.out.print(" | ");
         System.out.print("    DUE DATE");
         System.out.print(" | ");
-
         System.out.println();
-        System.out.println("----------------------------------------------------------------------------------"+
-                "---------------------------------");
-        System.out.println();
+        printDash();
 
     }
+    private void printInfoTop() {
+        printSalesPerson();
+        printShippingMethod();
+        printShippingTerms();
+        printDeliveryDate();
+        printPaymentTerms();
+        printDueDate();
 
+        System.out.println();
+        printDash();
+        System.out.println();
+    }
+
+    public void printDash(){
+        System.out.println("----------------------------------------------------------------------------------"+
+                "------------------------------------------------------");
+    }
+    public void printSalesPerson(){
+        System.out.print('|');
+        //PRINT SALES PERSON
+        int auxLen = 20-(this.salesPerson).length();
+        char [] aux = new char[auxLen];
+        for (int i=0; i<auxLen; i++){
+            aux[i]=' ';
+        }
+        String aux2 = new String(aux);
+        System.out.print(aux2+this.salesPerson);
+        System.out.print(" | ");
+    }
+    public void printShippingMethod(){
+        int auxLen = 23-(this.shippingMethod).length();
+        char [] aux = new char[auxLen];
+        for (int i=0; i<auxLen; i++){
+            aux[i]=' ';
+        }
+        String aux2 = new String(aux);
+        System.out.print(aux2+this.shippingMethod);
+        System.out.print(" | ");
+    }
+    private void printShippingTerms() {
+        int auxLen = 23-(this.shippingTerms).length();
+        char [] aux = new char[auxLen];
+        for (int i=0; i<auxLen; i++){
+            aux[i]=' ';
+        }
+        String aux2 = new String(aux);
+        System.out.print(aux2+this.shippingTerms);
+        System.out.print(" | ");
+    }
+
+    private void printDeliveryDate() {
+        System.out.print("         ");
+        //System.out.print(this.deliveryDate);
+        System.out.format("%tD", this.invoiceDate);
+        System.out.print(" | ");
+    }
+
+    private void printPaymentTerms() {
+        int auxLen = 23-(this.paymentTerms).length();
+        char [] aux = new char[auxLen];
+        for (int i=0; i<auxLen; i++){
+            aux[i]=' ';
+        }
+        String aux2 = new String(aux);
+        System.out.print(aux2+this.paymentTerms);
+        System.out.print(" | ");
+    }
+
+    private void printDueDate() {
+        System.out.print("    ");
+        //System.out.print(this.deliveryDate);
+        System.out.format("%tD", this.dueDate);
+        System.out.print(" | ");
+    }
 
 }
